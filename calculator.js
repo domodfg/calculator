119f515f985eadc76bd2e58e50fab6c5fb2bce22
domodfg <domodfg@yahoo.com.hk>
2022-07-09 02:59:31 +0800

    Add eventlistener to buttons and add value display

--- calculator.js
+++ calculator.js
@@ -19,13 +19,29 @@ function divide(number1, number2) {
 
 function operate(operator, number1, number2) {
   if (operator == "+") {
-    result= add(number1, number2);
+    result = add(number1, number2);
   } else if (operator == "-") {
-    result= subtract(number1, number2);
+    result = subtract(number1, number2);
   } else if (operator == "*") {
-    result= multiply(number1, number2);
+    result = multiply(number1, number2);
   } else if (operator == "/") {
-    result= divide(number1, number2);
+    result = divide(number1, number2);
   }
   return result;
 }
+let displayNum = '';
+const digitButtons = document.querySelectorAll('.digits');
+const displayer=document.querySelector('.displayNum');
+const clear = document.querySelector('.clear');
+
+digitButtons.forEach((digit) => {
+  digit.addEventListener('click', () => {
+    displayNum += digit.textContent;
+    displayer.textContent=displayNum;
+  });
+});
+
+clear.addEventListener('click' , () => {
+    displayer.textContent=0;
+    displayNum = '';
+})
