b3b12e9fc4db15a284e7aca325c6cfd781011c8c
domodfg <domodfg@yahoo.com.hk>
2022-07-09 02:36:22 +0800

    Add function for operate and add buttons to the UI

--- calculator.js
+++ calculator.js
@@ -16,3 +16,16 @@ function divide(number1, number2) {
     return number1 / number2;
   }
 }
+
+function operate(operator, number1, number2) {
+  if (operator == "+") {
+    result= add(number1, number2);
+  } else if (operator == "-") {
+    result= subtract(number1, number2);
+  } else if (operator == "*") {
+    result= multiply(number1, number2);
+  } else if (operator == "/") {
+    result= divide(number1, number2);
+  }
+  return result;
+}
