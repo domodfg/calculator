ea98d4ba88b117f3be586a9e5d45d5a71015f33f
domodfg <domodfg@yahoo.com.hk>
2022-07-09 20:16:52 +0800

    Add functions for chain operation and various bug fixes
    fix the bug that you can press an operator multiple times and
    returning NaN.

--- calculator.js
+++ calculator.js
@@ -18,7 +18,9 @@ function divide(number1, number2) {
 }
 
 function operate(operator, number1, number2) {
-  if (operator == "+") {
+  if (operator == "") {
+    return (result = number2);
+  } else if (operator == "+") {
     result = add(number1, number2);
   } else if (operator == "-") {
     result = subtract(number1, number2);
@@ -29,76 +31,72 @@ function operate(operator, number1, number2) {
   }
   return result;
 }
-let displayNum = '';
-let storeSecondNum='';
-const digitButtons = document.querySelectorAll('.digits');
-const displayer=document.querySelector('.displayNum');
-const clear = document.querySelector('.clear');
-const secondDisplayer=document.querySelector('.displaySecondNum');
-
+let displayNum = "";
+let storeSecondNum = "";
+const digitButtons = document.querySelectorAll(".digits");
+const displayer = document.querySelector(".displayNum");
+const clear = document.querySelector(".clear");
+const secondDisplayer = document.querySelector(".displaySecondNum");
 
 digitButtons.forEach((digit) => {
-  digit.addEventListener('click', () => {
+  digit.addEventListener("click", () => {
     displayNum += digit.textContent;
     storeSecondNum += digit.textContent;
-    displayer.textContent=displayNum;
-    secondDisplayer.textContent=storeSecondNum;
+    displayer.textContent = displayNum;
+    secondDisplayer.textContent = storeSecondNum;
   });
 });
 
-clear.addEventListener('click' , () => {
-    displayer.textContent=0;
-    secondDisplayer.textContent=0;
-    displayNum = '';
-    storeSecondNum='';
-})
-
-
+clear.addEventListener("click", () => {
+  displayer.textContent = "";
+  secondDisplayer.textContent = "";
+  displayNum = "";
+  storeSecondNum = "";
+  result = number2;
+  operator = "";
+});
 
 let number1;
 let number2;
-let operator;
-function storeNumber () {
-  number1= parseFloat(displayNum);
-  secondDisplayer.textContent=number1;
-  storeSecondNum='';
-
+let operator = "";
+function storeNumber() {
+  number1 = parseFloat(displayNum);
+  storeSecondNum = "";
 }
-function pressPlus() {
-  storeNumber();
-  operator='+';
+function pressEqual() {
+  if (storeSecondNum != "" && storeSecondNum != 'undefined') {
+    number2 = parseFloat(storeSecondNum);
+    operate(operator, number1, number2);
+    if (result == 'undefined') {
+      displayNum = result;
+      storeSecondNum = result;
+    } else {
+    displayNum = result.toFixed(2).replace(/[.,]00$/, "");
+    storeSecondNum = result.toFixed(2).replace(/[.,]00$/, "");
+    }
+    displayer.textContent = displayNum;
+    secondDisplayer.textContent = storeSecondNum;
+    operator = "";
+  }
 }
-function pressMinus() {
-  storeNumber();
-  operator='-';
-}
-function pressMultiply() {
-  storeNumber();
-  operator='*';
-}
-function pressDivide() {
-  storeNumber();
-  operator='/';
-}
-
-
-function pressEqual () {
-  number2= parseFloat(storeSecondNum);
-  operate(operator, number1, number2);
-  displayNum=result;
-  storeSecondNum=result;
-  displayer.textContent=displayNum;
-  secondDisplayer.textContent=storeSecondNum;
+function pressOperator(sign) {
+  if (storeSecondNum != "" && storeSecondNum != 'undefined') {
+    pressEqual();
+    storeNumber();
+    displayNum += sign;
+    displayer.textContent = displayNum;
+    operator = sign;
+  }
 }
 
-const plus = document.querySelector('.add');
-const minus = document.querySelector('.minus');
-const times = document.querySelector('.multiply');
-const over = document.querySelector('.divide');
-const equal = document.querySelector('.equal');
+const plus = document.querySelector(".add");
+const minus = document.querySelector(".minus");
+const times = document.querySelector(".multiply");
+const over = document.querySelector(".divide");
+const equal = document.querySelector(".equal");
 
-plus.addEventListener('click', pressPlus);
-minus.addEventListener('click', pressMinus);
-times.addEventListener('click', pressMultiply);
-over.addEventListener('click', pressDivide);
-equal.addEventListener('click', pressEqual);
+plus.addEventListener("click", () => pressOperator('+') );
+minus.addEventListener("click", () => pressOperator('-'));
+times.addEventListener("click", () => pressOperator('*'));
+over.addEventListener("click", () => pressOperator('/'));
+equal.addEventListener("click", pressEqual);
